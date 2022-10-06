39be75bb0c04182101effa670aa9b3dbcc2c0b03
Landry <31054293+landosf@users.noreply.github.com>
2022-10-05 17:46:23 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '829f4b648533411dae8e64e5e208a2b5',
+  rootNotionPageId: '7875426197cf461698809def95960ebf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Keep Movin',
-  domain: 'landosf.github.io/
-nextjs-notion-starter-kit',
-  author: 'Landry Yoder',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Mountains and Moons',
+  description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: '@landryyoder',
-  github: 'landosf',
-  linkedin: 'landryyoder',
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
